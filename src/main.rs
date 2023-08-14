e57fdb5e651cac82bf18dce11225a7c3883b4d59
k4yp <k4ypos@proton.me>
2023-08-14 22:52:50 +1000

    multi event stakes

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,29 @@ struct Arbitrage {
 impl Arbitrage {
 
     // calculates the stakes for each event
-    fn stakes(&self) -> Vec<Vec<f32>> {
-        let mut bookmaker_stakes = Vec::new();
-        for i in 0..self.odds.len() {
-            let mut event_stakes = Vec::new();
-            for j in 0..self.odds[0].len() {
-
-                let sum: f32 = self.odds.iter().map(|pair| 1.0/ pair[j]).sum();
-
-                event_stakes.push((1.0 / self.odds[i][0]) / sum * self.bet);
-            }
-            bookmaker_stakes.push(event_stakes);
-        }
-        bookmaker_stakes
-    }
-
-    // chooses the optimal bookmaker for each event
-    fn optimal(&self) -> Vec<f32> {
+    fn stakes(&self) -> Vec<f32> {
         let mut optimal = Vec::new();
-        for i in 0..self.odds.len() {
+        for i in 0..self.odds[0].len() {
             let mut choices = Vec::new();
-            for j in 0..self.odds[0].len() {
+            for j in 0..self.odds.len() {
                 choices.push(self.odds[j][i])
             }
             optimal.push(choices.into_iter().max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap());
         }
-        optimal 
+
+        let mut stakes: Vec<f32> = Vec::new();
+
+        for i in 0..optimal.len() {
+            let sum: f32 = optimal.iter().map(|odd| 1.0 / odd).sum();
+
+            stakes.push((1.0 / optimal[i]) * 100.0 / sum);
+        }
+        stakes
     }
 
     // total payout of the arbitrage including the initial bet
     fn payout(&self) -> f32 {
-        Arbitrage::stakes(&self)[0][0] * self.odds[0][0]
+        Arbitrage::stakes(&self)[0] * self.odds[0][0]
     }
 
     // total payout minus the initial bet
@@ -61,12 +53,11 @@ async fn main() -> WebDriverResult<()> {
 
     driver.goto("https://www.ladbrokes.com.au/racing/newcastle/4e4e11b4-25f9-4111-91d6-b60b11305c69").await?;
 
-    let bet = Arbitrage{bet: 50.0, odds: vec![[8.0, 1.08], [7.5, 1.09]]};
-    let optimal = bet.optimal();
+    let bet = Arbitrage{bet: 50.0, odds: vec![[3.2, 2.1], [4.2, 2.2], [5.3, 1.9]]};
 
     println!("{:?}", bet.stakes());
-    println!("Payout: ${:.2}\nProfit: ${:.2}", bet.payout(), bet.profit());
-    println!("ROI: {:.2}%", bet.roi());
+    // println!("Payout: ${:.2}\nProfit: ${:.2}", bet.payout(), bet.profit());
+    // println!("ROI: {:.2}%", bet.roi());
 
     driver.quit().await?;
 
