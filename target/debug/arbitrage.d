392bc8d65f7826ca89628470a5be6b55c4275b24
keyp0s <k4ypos@proton.me>
2023-08-14 16:19:35 +1000

    roi

---
+++ target/debug/arbitrage.d
@@ -0,0 +1,1 @@
+C:\Users\TUBM01\repos\arbitrage\target\debug\arbitrage.exe: C:\Users\TUBM01\repos\arbitrage\src\main.rs
---
+++ target/debug/deps/arbitrage-1fe1bba89609a76d.d
@@ -0,0 +1,5 @@
+C:\Users\TUBM01\repos\arbitrage\target\debug\deps\arbitrage-1fe1bba89609a76d.exe: src\main.rs
+
+C:\Users\TUBM01\repos\arbitrage\target\debug\deps\arbitrage-1fe1bba89609a76d.d: src\main.rs
+
+src\main.rs:
