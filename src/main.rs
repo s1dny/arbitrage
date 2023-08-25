ed5ffbb920801b840c8e2f20a927c84d0e416ac1
k4yp <k4ypos@proton.me>
2023-08-26 09:56:09 +1000

    works with tuples

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,18 @@
 use reqwest::blocking::Client;
 use serde_json::Value;
-use std::collections::HashMap;
 
 struct Arbitrage {
     bet: f64,
     round: f64,
-    odds: HashMap<String, (String, f64)>,
-    stakes: HashMap<String, (String, f64)>
+    odds: Vec<(String, (String, f64))>,
+    stakes: Vec<(String, (String, f64))>
 }
-
 
 impl Arbitrage {
     fn payout(&self) -> f64 {
-        if (self.odds.len() < 2) || (self.stakes.len() < 2) {
-            return 0.0;
-        }
         let mut total = 0.0;
-        for (bookmaker, _) in &self.odds{
-            total += self.stakes.get(bookmaker).unwrap().1 * self.odds.get(bookmaker).unwrap().1
+        for i in 0..self.odds.len(){
+            total += self.stakes[i].1.1 * self.odds[i].1.1;
         }
         total / self.odds.len() as f64 * self.bet
     }
@@ -31,7 +26,7 @@ impl Arbitrage {
     }
 }
 
-fn optimal2(odds: Vec<(String, Vec<(String, f64)>)>) -> Vec<(String, (String, f64))> {
+fn optimal(odds: Vec<(String, Vec<(String, f64)>)>) -> Vec<(String, (String, f64))> {
     let mut optimal = vec![(String::new(), (String::new(), 0.0)); odds[0].1.len()];
 
     for i in 0..odds.len() {
@@ -47,36 +42,15 @@ fn optimal2(odds: Vec<(String, Vec<(String, f64)>)>) -> Vec<(String, (String, f6
     optimal
 }
 
-fn optimal(odds: HashMap<String, ((String, f64), (String, f64))>) -> HashMap<String, (String, f64)> {
-    let mut team_1 = HashMap::from([(String::new(), (String::new(), 0.0))]);
-    let mut team_2 = HashMap::from([(String::new(), (String::new(), 0.0))]);
-
-    for (bookmaker, game) in &odds {
-        if game.0.1 > team_1.values().next().unwrap().1 {
-            team_1 = HashMap::from([(bookmaker.clone(), (game.0.0.clone(), game.0.1))]);
-        }
-        if game.1.1 > team_2.values().next().unwrap().1 {
-            team_2 = HashMap::from([(bookmaker.clone(), (game.1.0.clone(), game.1.1))]);
-        }
-    }
-
-    let mut combined = HashMap::new();
-    combined.extend(team_1);
-    combined.extend(team_2);
-
-    combined
-}
-
-
-fn stakes(odds: HashMap<String, (String, f64)>) -> HashMap<String, (String, f64)>  {
-    let mut stakes: HashMap<String, (String, f64)> = HashMap::new();
+fn stakes(odds: Vec<(String, (String, f64))>) -> Vec<(String, (String, f64))> {
+    let mut stakes: Vec<(String, (String, f64))> = Vec::new();
     let mut sum = 0.0;
 
     for (_, game) in &odds{
         sum += 1.0 / game.1
     }
     for (bookmaker, game) in &odds{
-        stakes.insert(bookmaker.clone(), (game.0.to_string(), (1.0 / game.1) / sum));
+        stakes.push((bookmaker.clone(), (game.0.to_string(), (1.0 / game.1) / sum)));
     }
     stakes
 }
@@ -92,10 +66,8 @@ fn main() {
         .json::<Value>()
         .unwrap();
 
-    for i in 0..1 {
-        let mut odds: HashMap<String, ((String, f64), (String, f64))> = HashMap::new();
-
-        let mut odds2: Vec<(String, Vec<(String, f64)>)> = Vec::new();
+    for i in 0..20 {
+        let mut odds: Vec<(String, Vec<(String, f64)>)> = Vec::new();
         for j in 0..10 {
             let bookmaker = &bets[i]["bookmakers"][j]["markets"][0]["outcomes"];
             let name = &bets[i]["bookmakers"][j]["key"];
@@ -117,42 +89,19 @@ fn main() {
                             }
                         }
                     }
-
-                    let outcome_0 = bookmaker[0]["name"].as_str();
-                    let price_0 = bookmaker[0]["price"].as_f64();
-
-                    let outcome_1 = bookmaker[1]["name"].as_str();
-                    let price_1 = bookmaker[1]["price"].as_f64();
-
-                    if let (Some(outcome_0), Some(price_0), Some(outcome_1), Some(price_1)) = (outcome_0, price_0, outcome_1, price_1) {
-                        odds.insert(
-                            name.to_string(),
-                            (
-                                (outcome_0.to_string(), price_0),
-                                (outcome_1.to_string(), price_1)
-                            )
-                        );
-                    } else {
-                        continue;
-                    }
                 }
-            odds2.push((name.to_string(), event));
+            odds.push((name.to_string(), event));
         }
-
-        let optimal2 = optimal2(odds2.clone());
-
-        println!("{:?}", optimal2);
 
         let optimal = optimal(odds.clone());
         let stakes = stakes(optimal.clone());
 
-        let bets = Arbitrage{
-                                bet: 500.0,
+        let bets = Arbitrage {
+                                bet: 100.0,
                                 round: 5.0,
                                 odds: optimal.clone(),
                                 stakes: stakes
-                            };
-
+        };
         let roi = bets.roi();
         let color_code = if roi > 0.0 { "\x1b[32m" } else { "\x1b[31m" };
 
